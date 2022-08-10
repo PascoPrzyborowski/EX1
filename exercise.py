7ced5513ac25a23a8aa5f03905791a1ab9c26ecb
PascoPrzyborowski <unity01@gmx.net>
2022-08-10 16:16:02 +0200

    Ex1

--- exercise.py
+++ exercise.py
@@ -1,3 +1,72 @@
 # do exercise here
 # upload to github for portfolio
 # thank me later when your git graph is as green as the python money u gonna earn
+
+
+import time
+
+
+# Playing with primitive data types
+print("Task 1")
+print()
+#time.sleep(1)
+print(234)
+print(type(234))
+#time.sleep(1)
+print(43.12)
+print(type(43.12))
+#time.sleep(1)
+print(3+2j)
+print(type(3+2j))
+#time.sleep(1)
+print('hello')
+print(type('hello'))
+#time.sleep(1)
+print(True)
+print(type(True))
+#time.sleep(1)
+print('\n')
+
+#Task 2
+print(123, 'istype of', type(123))
+print(43.23, 'istype of', type(43.23))
+print(4-1j, 'istype of', type(4-1j))
+print('How are you', 'istype of', type('How are you'))
+print(True, 'istype of', type(True))
+print('\n')
+
+
+#Task3
+print(isinstance(123, int))
+print(isinstance(123, float))
+print(isinstance(True, bool))
+print(isinstance(5+2j, complex))
+print(isinstance(123, str))
+print('\n')
+
+#TASK 4
+print('Is 123  an instance of int?: ',isinstance(123, int))
+print('Is 43.23  an instance of bool?:',isinstance(43.23, bool))
+print('Is (4-1j)  an instance of complex?: ',isinstance(4-1j, complex))
+print('Is True  an instance of bool?: ',isinstance(True, bool))
+print("Is 'How are you?'  an instance of float?: ",isinstance('How are you?', float))
+print('\n')
+
+# This is my first comment
+# Block comments are indented to the same level as that code
+print("Hello")
+print("Line with inline code!")  # remember about spacing!
+print(type(123.45))  # getting type of number
+"""
+You can use triple-quoted strings. When they're not a docstring (the first thing in a class/function/module), they are ignored.
+Read aforementioned answer from Stack Overflow!
+"""
+
+
+
+
+
+
+
+
+
